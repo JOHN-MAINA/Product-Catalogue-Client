da7c7637429e5d0304f6592ba1fd188fff300c96
John Maina <johnmaina109@gmail.com>
2018-10-06 09:18:00 +0300

    [wip]design ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { MaterialModule} from './material';
@@ -12,7 +13,8 @@ import { MaterialModule} from './material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,12 @@
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 import { NgModule} from '@angular/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatCheckboxModule]
+    imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule]
 })
 export class MaterialModule { }
