bb8a1a102cb11192159272cb9ccfc1a9b024373b
JOHN MAINA <jqhnmaina@outlook.com>
2018-10-21 21:32:53 +0300

    implement search component unit tests

--- src/app/components/templates/search/search.component.spec.ts
+++ src/app/components/templates/search/search.component.spec.ts
@@ -4,10 +4,13 @@ import {SearchComponent} from './search.component';
 import {MaterialModule} from '../../../material';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let searchEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,10 +23,27 @@ describe('SearchComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+    searchEl = fixture.debugElement.query(By.css('button'));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should raise searchEvent when clicked', () => {
+    const expectedPhrase = 'Search Phrase';
+    component.search = expectedPhrase;
+    component.emitSearchEvent();
+    component.searchEvent.subscribe(searchPhrase => {
+      expect(expectedPhrase).toEqual(searchPhrase);
+    });
+  });
+
+  it('submit button should initially be disabled', () => {
+    component.search = '';
+    fixture.detectChanges();
+    expect(searchEl.nativeElement.disabled).toBeTruthy();
+  });
+
 });
